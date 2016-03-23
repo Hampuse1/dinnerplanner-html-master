983dde3215fa0d7953e0b6fa2ddeb7afc7cb5168
Hampuse1 <hampuse1@hotmail.com>
2016-03-23 15:52:21 +0100

    Added files via upload

---
+++ js/view/view1.js
@@ -0,0 +1,10 @@
+//ExampleView Object constructor
+var View1 = function (container,model) {
+	
+	// Get all the relevant elements of the view (ones that show data
+  	// and/or ones that responed to interaction)
+	this.makedinnerButton = container.find("#makedinnerButton");
+	
+	
+}
+ 
